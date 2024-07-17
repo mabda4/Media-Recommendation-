3365bc1cb1a4eecc44192e1f79f20d3317b85e7a
Camille Kuwahara <kuwahara.camille@gmail.com>
2024-07-17 15:51:15 -0500

    Added Flask server

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+# # List of users kept in a set
+# valid_users = {'tom', 'bill' }
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/checklists", methods=["POST"])
+def checklists():
+    print("entered api")
+    
+    #Get the type of media and the content
+    media = request.form.get('media')
+    content = request.form.get('content')
+    #Checks if the user has input a media or content
+    if media and content:
+        response = {
+            "media": media,
+            "content": content
+        }
+        return jsonify(response)
+    else:
+        return jsonify({"error": "Missing media or content"}), 400
+
+
+if __name__== '__main___':
+    app.run(debug=True, host="0.0.0.0")
