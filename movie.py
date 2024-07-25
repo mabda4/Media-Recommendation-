ad767f939f2d8ad1957e4ee3e425d092ca823ecb
mabda4 <129088894+mabda4@users.noreply.github.com>
2024-07-24 19:55:34 -0500

    Create movie.py

---
+++ movie.py
@@ -0,0 +1,45 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("TMBD_API_KEY")
+
+def getTrailers(recommendations):
+    trailers = []
+    print(recommendations)
+    for movie_name in recommendations.split('\n'):
+        movie_name = movie_name.replace(' ', '+')
+        movie_name = movie_name[3:]
+        
+        endpoint = f"https://api.themoviedb.org/3/search/movie?query={movie_name}&api_key={api_key}"
+        
+
+        r = requests.get(endpoint)
+        data = r.json()
+
+        if not data['results']:
+            print(f"No results found for {movie_name}")
+            continue
+
+        movie_id = data['results'][0]['id']
+
+        endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={api_key}"
+
+        r = requests.get(endpoint)
+        data = r.json()
+
+        trailer_id = None
+
+        for result in data['results']:
+            if result['type'] == 'Trailer':
+                trailer_id = result['key']
+                break
+
+        if trailer_id is None:
+            continue
+
+        yt_url = f"https://www.youtube.com/embed/{trailer_id}"
+        trailers.append(yt_url)
+
+    return(trailers)
